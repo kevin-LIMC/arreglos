30efa9bf9fe7cd74907c1f211715e29b128e9ca5
Kevin Daniel Vazquez Vazquez <kevin09dvv@gmail.com>
2024-08-27 19:42:08 -0600

    Agregar archivos de proyecto.

---
+++ arreglos.py
@@ -0,0 +1,22 @@
+
+print("serie de arreglos")
+
+numeros = [10, 20, 30, 40, 50, 60]
+n = [3, 4, 5]
+c = ['L', 'U', 'I', 'S']
+s = "MONA LISA"
+
+
+print(numeros[5])
+print(n[1])
+print(c[3])
+
+
+print(len(s))  
+
+
+print(len(n))  
+
+
+print(len(numeros))
+
